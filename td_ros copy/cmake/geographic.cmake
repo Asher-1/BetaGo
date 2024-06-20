61ebcd5dba6b577309ddd5ef5b04054e2a3bf81f
Dahai <ludahai19@163.com>
2024-06-20 10:54:27 +0800

    remove dummy td_ros

--- td_ros copy/cmake/geographic.cmake
+++
@@ -1,3 +0,0 @@
-add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/GeographicLib)
-include_directories(${PROJECT_SOURCE_DIR}/third_party/GeographicLib/include/)
-list(APPEND GeoGraphic_LIBRARIES libGeographiccc)
--- td_ros copy/script/bag2csv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rostopic echo -b rosbag.bag -p /ridgeback_velocity_controller/odom/twist/twist/linear/x > topic1.txt
